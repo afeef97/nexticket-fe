1144f64fcc2a7a059909f619089cdda6b1a5108b
Afeef <afeef9706@gmail.com>
2024-01-10 16:46:03 +0800

    feat: RegisterForm component without API
    feat: added form and input from shadcn
    
    feat: installed valibot
    
    chore: sort imports
    
    feat: TextInputField component
    
    feat: added message to text input
    
    feat: RegisterForm with client side validation

---
+++ src/components/Auth/RegisterForm.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import {
+  email,
+  maxLength,
+  minLength,
+  object,
+  regex,
+  string,
+  Input as vInput,
+} from 'valibot';
+import { Button } from '../ui/button';
+import { Form } from '@/components/ui/form';
+import TextInputField from '../shared/TextInputField';
+import { useForm } from 'react-hook-form';
+import { valibotResolver } from '@hookform/resolvers/valibot';
+
+export const RegisterFormSchema = object({
+  email: string([email()]),
+  password: string([
+    minLength(8, 'Password must be at least 8 characters'),
+    maxLength(30, 'Your password is too long.'),
+    regex(/[a-z]/, 'Your password must contain a lowercase letter.'),
+    regex(/[A-Z]/, 'Your password must contain an uppercase letter.'),
+    regex(/[0-9]/, 'Your password must contain a number.'),
+  ]),
+});
+
+const RegisterForm = () => {
+  const registerForm = useForm<vInput<typeof RegisterFormSchema>>({
+    resolver: valibotResolver(RegisterFormSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  return (
+    <Form {...registerForm}>
+      <form onSubmit={registerForm.handleSubmit((data) => console.log(data))}>
+        <TextInputField
+          control={registerForm.control}
+          name='email'
+          label='Email'
+        />
+        <TextInputField
+          control={registerForm.control}
+          name='password'
+          label='Password'
+        />
+
+        <Button type='submit'>Register</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default RegisterForm;
