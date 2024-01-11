680988bbb12f5957be94a41a2a220bd4bab60022
Afeef <afeef9706@gmail.com>
2024-01-11 16:37:14 +0800

    feat: added password confirmation

--- src/components/Auth/RegisterForm.tsx
+++ src/components/Auth/RegisterForm.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import {
+  custom,
   email,
+  forward,
   maxLength,
   minLength,
   object,
@@ -16,16 +18,28 @@ import TextInputField from '../shared/TextInputField';
 import { useForm } from 'react-hook-form';
 import { valibotResolver } from '@hookform/resolvers/valibot';
 
-export const RegisterFormSchema = object({
-  email: string([email()]),
-  password: string([
-    minLength(8, 'Password must be at least 8 characters'),
-    maxLength(30, 'Your password is too long.'),
-    regex(/[a-z]/, 'Your password must contain a lowercase letter.'),
-    regex(/[A-Z]/, 'Your password must contain an uppercase letter.'),
-    regex(/[0-9]/, 'Your password must contain a number.'),
-  ]),
-});
+export const RegisterFormSchema = object(
+  {
+    email: string([email()]),
+    password: string([
+      minLength(8, 'Password must be at least 8 characters'),
+      maxLength(30, 'Your password is too long.'),
+      regex(/[a-z]/, 'Your password must contain a lowercase letter.'),
+      regex(/[A-Z]/, 'Your password must contain an uppercase letter.'),
+      regex(/[0-9]/, 'Your password must contain a number.'),
+    ]),
+    confirmPassword: string([minLength(1, 'Enter your password again')]),
+  },
+  [
+    forward(
+      custom(
+        (input) => input.password === input.confirmPassword,
+        'The passwords do not match'
+      ),
+      ['confirmPassword']
+    ),
+  ]
+);
 
 const RegisterForm = () => {
   const registerForm = useForm<vInput<typeof RegisterFormSchema>>({
@@ -33,6 +47,7 @@ const RegisterForm = () => {
     defaultValues: {
       email: '',
       password: '',
+      confirmPassword: '',
     },
   });
 
@@ -56,6 +71,13 @@ const RegisterForm = () => {
             placeholder='Enter your password'
             autoComplete='password'
           />
+        </TextInputField>
+        <TextInputField
+          control={registerForm.control}
+          label='Confirm password'
+          name='confirmPassword'
+        >
+          <Input type='password' placeholder='Enter your password again' />
         </TextInputField>
 
         <Button type='submit'>Register</Button>
