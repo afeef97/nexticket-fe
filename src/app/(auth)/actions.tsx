f3eaa4b1637db6146156278562127e5bdb1a8c6f
Afeef <afeef9706@gmail.com>
2024-01-16 13:42:00 +0800

    App layout with AccessExpire handler
    feat: added getUserAccount server action
    
    fix: fetch wrapper get token
    
    feat: added logout user server action
    
    feat: access expiry page
    
    feat: redirect verify success to dashboard
    
    feat: added dialog from shadcn
    
    feat: added refreshToken action and expire cookie
    
    feat: changed accessexpired into dialog
    
    refactor: created AccessExpired context
    
    feat: throw fetch wrapper if cookie expire
    
    wip: dashboard page
    Signed-off-by: Afeef <afeef9706@gmail.com>

--- src/app/(auth)/actions.tsx
+++ src/app/(auth)/actions.tsx
@@ -1,17 +1,19 @@
 'use server';
 
+import fetchNexticket from '@/lib/customFetch';
+import { handleAsyncQuery } from '@/lib/utils';
+
 export const handleResponseCookies = (setCookie: string[]) => {
   if (setCookie.length > 0) {
     const { cookies } = require('next/headers');
     const cookieStore = cookies();
 
-    setCookie.map((cookie: string) => {
+    setCookie.forEach((cookie: string) => {
       const cookieValue = cookie.split(';');
 
-      const tokenLabel = cookieValue[0].split('=')[0];
-      const token = cookieValue[0].split('=')[1];
-      const path = cookieValue[1].split('=')[1];
-      const expires = cookieValue[2].split('=')[1];
+      const [tokenLabel, token] = cookieValue[0].split('=');
+      const [, path] = cookieValue[1].split('=');
+      const [, expires] = cookieValue[2].split('=');
 
       cookieStore.set(tokenLabel, token, {
         path,
@@ -19,6 +21,47 @@ export const handleResponseCookies = (setCookie: string[]) => {
         httpOnly: true,
         sameSite: 'strict',
       });
+
+      cookieStore.set(`${tokenLabel}_expires`, expires);
     });
   }
+};
+
+export const refreshToken = async () => {
+  const { cookies } = require('next/headers');
+  const cookieStore = cookies();
+
+  console.log(cookieStore);
+  if (!cookieStore.has('refresh_token')) {
+    return;
+  }
+
+  return await handleAsyncQuery(() =>
+    fetchNexticket('/auth/refresh', {
+      useToken: false,
+      method: 'POST',
+      options: {
+        headers: {
+          Authorization: `Bearer ${cookieStore.get('refresh_token')?.value}`,
+        },
+      },
+    })
+  );
+};
+
+export const logoutUser = async () => {
+  const { cookies } = require('next/headers');
+  const cookieStore = cookies();
+
+  const response = await handleAsyncQuery(() =>
+    fetchNexticket('/auth/logout', { options: { cache: 'no-store' } })
+  );
+
+  if (!response.ok) {
+    return response;
+  }
+
+  cookieStore.delete('access_token');
+  cookieStore.delete('refresh_token');
+  return response;
 };
