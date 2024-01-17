928ef3c4df33779261d0c9152fbe83d3b2bcbd88
Afeef <afeef9706@gmail.com>
2024-01-17 14:08:32 +0800

    Added token expiry handler to fetch wrapper
    chore: access context typing
    
    refactor: moved token handler to its own function
    
    refactor: cleaned up fetch wrapper
    Signed-off-by: Afeef <afeef9706@gmail.com>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,39 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { FetchReturn } from './customFetch';
+import { IAccessContext } from '@/components/shared/AccessExpired';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function tokenHandler(
+  context: IAccessContext | undefined,
+  options: RequestInit
+): FetchReturn | undefined {
+  const { cookies } = require('next/headers');
+  const cookieStore = cookies();
+
+  const accessExpires: string = cookieStore.get('access_token_expires')?.value;
+  if (
+    accessExpires &&
+    new Date(Date.now()).valueOf() > new Date(accessExpires).valueOf()
+  ) {
+    if (context && typeof window !== 'undefined') {
+      context.setOpenAccessExpired(true);
+    }
+    return {
+      ok: false,
+      data: {
+        message: 'Your access token has expired, please refresh your token',
+        statusCode: 401,
+      },
+    };
+  }
+
+  const token = cookieStore.get('access_token')?.value;
+  options.headers = {
+    ...options.headers,
+    Authorization: `Bearer ${token}`,
+  };
 }
