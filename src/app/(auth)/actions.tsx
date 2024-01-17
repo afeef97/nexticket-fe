9c55430c93742677a085a8e6f50edabedcd8448c
Afeef <afeef9706@gmail.com>
2024-01-17 11:46:42 +0800

    Reworked fetch wrapper return
    refactor: fetch now return ok and data, removed query handler
    
    refactor: removed query handler from server actions
    Signed-off-by: Afeef <afeef9706@gmail.com>

--- src/app/(auth)/actions.tsx
+++ src/app/(auth)/actions.tsx
@@ -1,7 +1,6 @@
 'use server';
 
 import fetchNexticket from '@/lib/customFetch';
-import { handleAsyncQuery } from '@/lib/utils';
 
 export const handleResponseCookies = (setCookie: string[]) => {
   if (setCookie.length > 0) {
@@ -31,31 +30,29 @@ export const refreshToken = async () => {
   const { cookies } = require('next/headers');
   const cookieStore = cookies();
 
-  console.log(cookieStore);
   if (!cookieStore.has('refresh_token')) {
     return;
   }
 
-  return await handleAsyncQuery(() =>
-    fetchNexticket('/auth/refresh', {
-      useToken: false,
-      method: 'POST',
-      options: {
-        headers: {
-          Authorization: `Bearer ${cookieStore.get('refresh_token')?.value}`,
-        },
+  return await fetchNexticket('/auth/refresh', {
+    useToken: false,
+    method: 'POST',
+    options: {
+      headers: {
+        Authorization: `Bearer ${cookieStore.get('refresh_token')?.value}`,
       },
-    })
-  );
+    },
+  });
 };
 
 export const logoutUser = async () => {
   const { cookies } = require('next/headers');
   const cookieStore = cookies();
 
-  const response = await handleAsyncQuery(() =>
-    fetchNexticket('/auth/logout', { options: { cache: 'no-store' } })
-  );
+  const response = await fetchNexticket('/auth/logout', {
+    options: { cache: 'no-store' },
+  });
+  
 
   if (!response.ok) {
     return response;
--- src/app/dashboard/actions.tsx
+++ src/app/dashboard/actions.tsx
@@ -1,18 +1,15 @@
 'use server';
 
 import fetchNexticket from '@/lib/customFetch';
-import { handleAsyncQuery } from '@/lib/utils';
 
 export const getUserAccount = async () => {
-  return await handleAsyncQuery(() =>
-    fetchNexticket('/users/my-account', {
-      useToken: true,
-      options: {
-        credentials: 'include',
-        next: {
-          tags: ['user-account'],
-        },
+  return await fetchNexticket('/users/my-account', {
+    useToken: true,
+    options: {
+      credentials: 'include',
+      next: {
+        tags: ['user-account'],
       },
-    })
-  );
+    },
+  });
 };
