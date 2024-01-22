30a14187fe0a5f89472773d98f90c814113dc151
Afeef <afeef9706@gmail.com>
2024-01-22 10:20:00 +0800

    Mobile header responsiveness
    refactor: removed dependency on window innerwidth
    
    feat: disable navigation links if no organization

--- src/app/dashboard/(dashboard)/NavigationPanel.tsx
+++ src/app/dashboard/(dashboard)/NavigationPanel.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+import { FetchReturn } from '@/lib/customFetch';
 import { ILink } from './Navigation';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
+import { getUserAccount } from '../actions';
 import { usePathname } from 'next/navigation';
 import useWindowInnerWidth from '@/lib/hooks/useWindowInnerWidth';
 
@@ -10,33 +13,41 @@ const NavigationPanel = ({
   showPanel,
   links = [],
 }: {
-  showPanel: boolean;
+  showPanel: boolean | undefined;
   links: ILink[];
 }) => {
   const pathname = usePathname();
   const windowInnerWidth = useWindowInnerWidth();
+
+  const [userData, setUserData] = useState<FetchReturn>();
+  const getUserAccountOnMount = async () => {
+    const response = await getUserAccount();
+    setUserData(response);
+  };
+  useEffect(() => {
+    getUserAccountOnMount();
+  }, []);
 
   return (
     <>
       <div
         data-testid='navigation-panel'
         className={cn(
-          'tw-flex tw-flex-col tw-items-center md:tw-items-start tw-fixed md:tw-static tw-bottom-0 tw-z-50 tw-rounded-[0.75rem_0.75rem_0_0] md:tw-rounded-none tw-py-4 tw-px-6 tw-w-full md:tw-w-52 tw-h-40 md:tw-min-h-screen tw-transition-transform md:tw-transition-none tw-duration-300 tw-shadow-lg tw-shadow-black md:tw-shadow-none md:tw-border-r md:tw-border-black'
+          'tw-flex tw-flex-col tw-items-center md:tw-items-start tw-fixed md:tw-static tw-bottom-0 tw-z-50 tw-rounded-[0.75rem_0.75rem_0_0] md:tw-rounded-none tw-py-4 tw-px-6 tw-w-full md:tw-w-52 tw-h-40 md:tw-min-h-screen tw-transition-transform md:tw-transition-none tw-duration-300 tw-shadow-lg tw-shadow-black md:tw-shadow-none md:tw-border-r md:tw-border-black',
+          showPanel ? 'max-sm:tw-translate-y-0' : 'max-sm:tw-translate-y-full'
         )}
-        style={{ transform: showPanel ? 'translateY(0)' : 'translateY(100%)' }}
       >
-        {windowInnerWidth > 768 ? (
-          <h3 className='tw-self-center'>nexticket</h3>
-        ) : (
-          <div className='tw-bg-gray-300 tw-h-1 tw-w-32 tw-mb-1 tw-rounded-xl'></div>
-        )}
+        <h3 className='tw-self-center tw-hidden md:tw-block'>nexticket</h3>
+        <div className='tw-bg-gray-300 tw-h-1 tw-w-32 tw-mb-1 tw-rounded-xl md:tw-hidden' />
         <nav className='tw-w-full md:tw-mt-4'>
           {links.map((link) => (
             <Link
               key={link.label}
               href={link.href}
               className={cn(
-                'tw-py-2 md:tw-py-3  tw-w-full tw-flex tw-items-center tw-justify-end md:tw-justify-start tw-gap-3',
+                'tw-py-2 md:tw-py-3 tw-w-full tw-flex tw-items-center tw-justify-end md:tw-justify-start tw-gap-3',
+                !userData?.data?.data?.organization_id &&
+                  'tw-text-gray-500 tw-pointer-events-none',
                 pathname === link.href && 'tw-font-bold tw-pointer-events-none'
               )}
             >
