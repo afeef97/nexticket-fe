0c021cdc51cd426724d6c91ca5eec706a2eaa337
Afeef <afeef9706@gmail.com>
2024-01-24 15:02:08 +0800

    Revalidate user account after register organization
    Signed-off-by: Afeef <afeef9706@gmail.com>

--- src/app/dashboard/register-organization/actions.tsx
+++ src/app/dashboard/register-organization/actions.tsx
@@ -1,16 +1,20 @@
 'use server';
 
 import fetchNexticket from '@/lib/customFetch';
+import { revalidateTag } from 'next/cache';
 
 export const registerOrganization = async (
   name: string,
   email_domain: string
 ) => {
-  return await fetchNexticket('/organization', {
+  const response = await fetchNexticket('/organization', {
     method: 'POST',
     body: { name, email_domain },
     options: {
       cache: 'no-store',
     },
   });
+
+  if (response.ok) revalidateTag('user-account');
+  return response;
 };
