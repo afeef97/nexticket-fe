1da1ad9a9b8c16dc2ff45979a88f16ca46f4ccc6
Afeef <afeef9706@gmail.com>
2024-02-02 13:35:10 +0800

    Dashboard page basic
    feat: added card from shadcn
    
    feat: customFetch log option
    
    feat: margin for NotifyCreateApikey
    
    feat: getTickets server action
    
    feat: TicketTotal and TicketTypes component
    
    feat: updated getTickets
    
    feat: TicketTypes sizing
    
    feat: TicketPriority component
    
    refactor: fetch strategy
    
    fix: color
    
    feat: max height

--- src/app/dashboard/actions.tsx
+++ src/app/dashboard/actions.tsx
@@ -26,3 +26,46 @@ export const getOrganization = async () => {
     },
   });
 };
+
+export const getTickets = async () => {
+  const response = await fetchNexticket('/ticket', {
+    options: {},
+  });
+
+  let totalTickets: number = 0;
+  const categoriesMap: Map<string, number> = new Map();
+  const priorityMap: Map<string, number> = new Map();
+
+  if (response.ok) {
+    totalTickets = response.data.data.length;
+    response.data.data.map((ticket: any) => {
+      if (!priorityMap.has(ticket.priority) && ticket.priority) {
+        priorityMap.set(ticket.priority, 1);
+      } else if (priorityMap.has(ticket.priority)) {
+        priorityMap.set(
+          ticket.priority,
+          (priorityMap.get(ticket.priority) as number) + 1
+        );
+      }
+
+      if (!categoriesMap.has(ticket.category)) {
+        categoriesMap.set(ticket.category, 1);
+      } else if (categoriesMap.has(ticket.category)) {
+        categoriesMap.set(
+          ticket.category,
+          (categoriesMap.get(ticket.category) as number) + 1
+        );
+      }
+    });
+  }
+
+  return {
+    ok: response.ok,
+    data: {
+      ...response.data,
+      totalTickets,
+      categories: categoriesMap,
+      priorities: priorityMap,
+    },
+  };
+};
