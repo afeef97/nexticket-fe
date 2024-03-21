6c72107378e43e301ba362a4c5ce1716c2a44161
Afeef <contact@afeef.dev>
2024-03-21 17:16:56 +0800

    refactor: use action for verifyform

--- src/app/(auth)/verify/VerifyForm.tsx
+++ src/app/(auth)/verify/VerifyForm.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { EmptyResponse, FetchReturn } from '@/lib/types';
 import { minLength, object, string, Input as vInput } from 'valibot';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { Button } from '@/components/ui/button';
@@ -8,7 +9,7 @@ import { Input } from '@/components/ui/input';
 import TextInputField from '@/components/shared/TextInputField';
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-import useQueryHandler from '@/lib/hooks/useQueryHandler';
+import { useFormState } from 'react-dom';
 import { valibotResolver } from '@hookform/resolvers/valibot';
 import { verifyUser } from '@/app/(auth)/verify/actions';
 
@@ -19,46 +20,56 @@ const VerifyFormSchema = object({
 const VerifyForm = () => {
   const verifyForm = useForm<vInput<typeof VerifyFormSchema>>({
     resolver: valibotResolver(VerifyFormSchema),
+    mode: 'onChange',
     defaultValues: {
       token: '',
     },
   });
+
   const searchParams = useSearchParams();
   const router = useRouter();
-
   useEffect(() => {
     if (!searchParams.has('email')) {
       router.push('/');
     }
   }, [searchParams, router]);
 
-  const { state: verifyUserState, triggerQuery: triggerVerifyUser } =
-    useQueryHandler({
-      query: verifyUser,
-      queryOnMount: false,
-    });
-  const onSubmit = async (data: vInput<typeof VerifyFormSchema>) => {
-    const email = searchParams.get('email');
-    const response = await triggerVerifyUser(email, data.token);
-    if (!response.ok) {
-      verifyForm.setError('token', { message: response.data.message });
+  const [verifyFormState, verifyFormAction] = useFormState(
+    verifyUser,
+    {} as FetchReturn<EmptyResponse>
+  );
+  useEffect(() => {
+    if (!verifyFormState.data) return;
+
+    if (!verifyFormState.ok) {
+      verifyForm.clearErrors();
+      verifyForm.setError('token', {
+        message: verifyFormState.data.message,
+      });
       return;
     }
 
-    router.push('/dashboard');
-  };
+    router.replace('/dashboard');
+  }, [verifyForm, verifyFormState, router]);
 
   return (
     <Form {...verifyForm}>
-      <form onSubmit={verifyForm.handleSubmit(onSubmit)}>
-        <TextInputField control={verifyForm.control} label='Token' name='token'>
+      <form
+        action={(formData) => {
+          formData.append('email', searchParams.get('email') as string);
+          verifyFormAction(formData);
+        }}
+      >
+        <TextInputField
+          control={verifyForm.control}
+          label='Token'
+          name='token'
+        >
           <Input placeholder='Please enter your token' />
         </TextInputField>
 
         <Button
-          disabled={
-            verifyUserState === 'pending' || verifyUserState === 'resolved'
-          }
+          disabled={verifyFormState.ok || !verifyForm.formState.isValid}
           type='submit'
           className='w-full'
         >
