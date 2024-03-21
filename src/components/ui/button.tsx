e5c4e26fa0213c2d2c9e23a73fbb8e4381a42690
Afeef <contact@afeef.dev>
2024-03-21 15:06:46 +0800

    refactor: button with formstatus

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { cva, type VariantProps } from 'class-variance-authority';
+import { type VariantProps, cva } from 'class-variance-authority';
 import { Slot } from '@radix-ui/react-slot';
 
 import { cn } from '@/lib/utils';
+import { useFormStatus } from 'react-dom';
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
@@ -42,8 +43,10 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
+    const { pending } = useFormStatus();
     return (
       <Comp
+        disabled={pending || props.disabled}
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
