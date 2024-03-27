915ae7e8205c8dc2c06ee86da46977979c9f3fcc
Afeef <contact@afeef.dev>
2024-03-27 11:03:31 +0800

    Feature/update table semantic (#67)
    * feat: moved pagination to table footer
    
    * chore: removed fragment

--- src/app/parliament/(components)/ParliamentTable.tsx
+++ src/app/parliament/(components)/ParliamentTable.tsx
@@ -45,122 +45,129 @@ const ParliamentTable = ({
     router.push(`/parliament/complaint/${ticketID}`);
   };
   return (
-    <>
-      <table className='min-w-full '>
-        <thead>
-          <tr className='bg-baseBg'>
-            <th
-              scope='col'
-              className='py-3.5 pl-4 pr-3 text-left text-body1  text-textSecondary sm:pl-6 md:pl-3'
-            >
-              ID
-            </th>
-            <th
-              scope='col'
-              className='py-3.5 px-3 text-left text-body1  text-textSecondary'
-            >
-              Requested on
-            </th>
-            <th
-              scope='col'
-              className='py-3.5 px-3 text-left text-body1  text-textSecondary'
-            >
-              Title
-            </th>
-            <th
-              scope='col'
-              className='py-3.5 px-3 text-left text-body1  text-textSecondary w-[150px]'
-            >
-              Status
-            </th>
+    <table className='min-w-full '>
+      <thead>
+        <tr className='bg-baseBg'>
+          <th
+            scope='col'
+            className='py-3.5 pl-4 pr-3 text-left text-body1  text-textSecondary sm:pl-6 md:pl-3'
+          >
+            ID
+          </th>
+          <th
+            scope='col'
+            className='py-3.5 px-3 text-left text-body1  text-textSecondary'
+          >
+            Requested on
+          </th>
+          <th
+            scope='col'
+            className='py-3.5 px-3 text-left text-body1  text-textSecondary'
+          >
+            Title
+          </th>
+          <th
+            scope='col'
+            className='py-3.5 px-3 text-left text-body1  text-textSecondary w-[150px]'
+          >
+            Status
+          </th>
+        </tr>
+      </thead>
+      <tbody className='divide-y divide-lineSecondary'>
+        {/* normal state */}
+        {complaintTicketData.ok &&
+          complaintTicketData.data.data.data.map(
+            (item: ParliamentTickets, index: number) => (
+              <tr
+                onClick={() => handleRowClick(`${item.id}`)}
+                key={index}
+                className='cursor-pointer'
+              >
+                <td className='whitespace-nowrap py-4 pl-4 pr-3 text-body1 text-textPrimary sm:pl-6 md:pl-3'>
+                  {item.id}
+                </td>
+                <td className='whitespace-nowrap py-4 px-3 text-body1 text-textPrimary'>
+                  {new Date(item.created_at).toString().split('GMT')[0]}
+                </td>
+                <td className='whitespace-nowrap py-4 px-3 text-body1 text-textPrimary'>
+                  {item.title}
+                </td>
+                <td className='whitespace-nowrap py-4 px-3 text-body1 text-textPrimary flex items-center gap-2'>
+                  <span>
+                    {item.ticket_status === 'COMPLETED' && (
+                      <div className='w-[10px] h-[10px] rounded-full bg-positive'></div>
+                    )}
+                    {item.ticket_status === 'PENDING' && (
+                      <div className='w-[10px] h-[10px] rounded-full bg-warning'></div>
+                    )}
+                    {item.ticket_status === 'AWAITING_REPLY' && (
+                      <div className='w-[10px] h-[10px] rounded-full bg-waitingColor'></div>
+                    )}
+                    {item.ticket_status === 'REJECTED' && (
+                      <div className='w-[10px] h-[10px] rounded-full bg-negative'></div>
+                    )}
+                  </span>
+                  <span>
+                    {item.ticket_status === 'AWAITING_REPLY'
+                      ? 'Awaiting reply'
+                      : item.ticket_status}
+                  </span>
+                </td>
+              </tr>
+            )
+          )}
+        {/* error state */}
+        {((complaintTicketData.ok &&
+          complaintTicketData.data.data.data.length === 0) ||
+          !complaintTicketData.ok) && (
+          <tr>
+            <td colSpan={4}>
+              <ParliamentEmptyState />
+            </td>
           </tr>
-        </thead>
-        <tbody className='divide-y divide-lineSecondary'>
-          {/* normal state */}
-          {complaintTicketData.ok &&
-            complaintTicketData.data.data.data.map(
-              (item: ParliamentTickets, index: number) => (
-                <tr
-                  onClick={() => handleRowClick(`${item.id}`)}
-                  key={index}
-                  className='cursor-pointer'
-                >
-                  <td className='whitespace-nowrap py-4 pl-4 pr-3 text-body1 text-textPrimary sm:pl-6 md:pl-3'>
-                    {item.id}
-                  </td>
-                  <td className='whitespace-nowrap py-4 px-3 text-body1 text-textPrimary'>
-                    {new Date(item.created_at).toString().split('GMT')[0]}
-                  </td>
-                  <td className='whitespace-nowrap py-4 px-3 text-body1 text-textPrimary'>
-                    {item.title}
-                  </td>
-                  <td className='whitespace-nowrap py-4 px-3 text-body1 text-textPrimary flex items-center gap-2'>
-                    <span>
-                      {item.ticket_status === 'COMPLETED' && (
-                        <div className='w-[10px] h-[10px] rounded-full bg-positive'></div>
-                      )}
-                      {item.ticket_status === 'PENDING' && (
-                        <div className='w-[10px] h-[10px] rounded-full bg-warning'></div>
-                      )}
-                      {item.ticket_status === 'AWAITING_REPLY' && (
-                        <div className='w-[10px] h-[10px] rounded-full bg-waitingColor'></div>
-                      )}
-                      {item.ticket_status === 'REJECTED' && (
-                        <div className='w-[10px] h-[10px] rounded-full bg-negative'></div>
-                      )}
-                    </span>
-                    <span>
-                      {item.ticket_status === 'AWAITING_REPLY'
-                        ? 'Awaiting reply'
-                        : item.ticket_status}
-                    </span>
-                  </td>
-                </tr>
-              )
-            )}
-          {/* error state */}
-          {((complaintTicketData.ok &&
-            complaintTicketData.data.data.data.length === 0) ||
-            !complaintTicketData.ok) && (
+        )}
+      </tbody>
+      <tfoot>
+        {complaintTicketData.ok &&
+          complaintTicketData?.data.data.data.length !== 0 && (
             <tr>
               <td colSpan={4}>
-                <ParliamentEmptyState />
+                <ParliamentTicketingPagination
+                  count={complaintTicketData?.data.data.meta.total}
+                  lastPage={complaintTicketData?.data.data.meta.lastPage}
+                  pages={currentPage}
+                  rows={rowsPerCurrentPage}
+                  setPages={setCurrentPage}
+                  setRows={setRowsPerCurrentPage}
+                  from={
+                    complaintTicketData?.data.data.meta.currentPage *
+                      rowsPerCurrentPage -
+                    rowsPerCurrentPage +
+                    1
+                  }
+                  to={
+                    complaintTicketData?.data.data.meta.currentPage ===
+                    complaintTicketData?.data.data.meta.lastPage
+                      ? complaintTicketData?.data.data.meta.total
+                      : complaintTicketData?.data.data.meta.currentPage *
+                        rowsPerCurrentPage
+                  }
+                />
               </td>
             </tr>
           )}
-        </tbody>
-      </table>
-      {/* PAGINATION */}
-      {complaintTicketData.ok &&
-        complaintTicketData?.data.data.data.length !== 0 && (
-          <ParliamentTicketingPagination
-            count={complaintTicketData?.data.data.meta.total}
-            lastPage={complaintTicketData?.data.data.meta.lastPage}
-            pages={currentPage}
-            rows={rowsPerCurrentPage}
-            setPages={setCurrentPage}
-            setRows={setRowsPerCurrentPage}
-            from={
-              complaintTicketData?.data.data.meta.currentPage *
-                rowsPerCurrentPage -
-              rowsPerCurrentPage +
-              1
-            }
-            to={
-              complaintTicketData?.data.data.meta.currentPage ===
-              complaintTicketData?.data.data.meta.lastPage
-                ? complaintTicketData?.data.data.meta.total
-                : complaintTicketData?.data.data.meta.currentPage *
-                  rowsPerCurrentPage
-            }
-          />
-        )}
-      {!complaintTicketData.ok ||
-        (complaintTicketData.ok &&
-          complaintTicketData.data.data.data.length === 0 && (
-            <ParliamentTableBottom />
-          ))}
-    </>
+        {!complaintTicketData.ok ||
+          (complaintTicketData.ok &&
+            complaintTicketData.data.data.data.length === 0 && (
+              <tr>
+                <td colSpan={4}>
+                  <ParliamentTableBottom />
+                </td>
+              </tr>
+            ))}
+      </tfoot>
+    </table>
   );
 };
 
