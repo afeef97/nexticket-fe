0c0174ffb88b26ae1c00deb9bf2984c6eedde0b2
Afeef <contact@afeef.dev>
2024-04-03 13:56:52 +0800

    refactor: handle login redirect from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,12 @@
-import { type NextRequest } from 'next/server';
+import { type NextRequest, NextResponse } from 'next/server';
+import { FRONTEND_URL } from './lib/constants';
 import refreshIfExpires from './lib/middlewares/refreshIfExpires';
 import refreshIfNoToken from './lib/middlewares/refreshIfNoToken';
 
 export async function middleware(request: NextRequest) {
   const hasAccessToken = request.cookies.has('access_token');
   const hasRefeshToken = request.cookies.has('refresh_token');
+  const isLogin = request.nextUrl.pathname.startsWith('/login');
   const isProtectedRoute =
     request.nextUrl.pathname.startsWith('/dashboard') ||
     request.nextUrl.pathname.startsWith('/parliament');
@@ -14,6 +16,12 @@ export async function middleware(request: NextRequest) {
   }
 
   if (hasAccessToken && hasRefeshToken && isProtectedRoute) {
-    return refreshIfExpires(request);
+    return await refreshIfExpires(request);
   }
+
+  if (hasAccessToken && isLogin) {
+    return NextResponse.redirect(FRONTEND_URL + '/dashboard');
+  }
+
+  return NextResponse.next();
 }
